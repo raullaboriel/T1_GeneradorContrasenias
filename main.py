b967648556237ae72609c7a99f01ccbc4e8b5f6f
laexR <86560746+raullaboriel@users.noreply.github.com>
2021-07-25 13:26:43 +0000

    Cambie alredyIn que era una lista por un diccionario lo que permite un tiempo constante para la busqueda.

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 from random import randint
 
-def passwordGenerator():
-  Password = ''
-  AlredyIn = [];
+def generatePassword():
+  alredyIn = {}
+  password = ''
 
   for i in range (15):
-    Character = chr(randint(33, 125))
-    while(Character in AlredyIn):
-      Character = chr(randint(33, 125))
-    Password = Password + Character
-    AlredyIn.append(Character);
-  return Password
-  
-print(passwordGenerator())
+    character = chr(randint(33,125));
+    while(alredyIn.get(character) != None):
+     character = chr(randint(33,125));
+    password += character
+    alredyIn[character] = character
+  return password
+
+print(generatePassword())
